1869f3c31945c3a758e1b90ee13a619f3089db18
Ehak Math <ehakmath@gmail.com>
2025-10-31 11:21:22 +0700

    Update greeting message to Hello, CI/CD!

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
-const request = require("supertest");
-const app = require("../app");
-const { expect } = require("chai");
-describe("GET /", () => {
-  it("should return Hello, GitHub Actions!", async () => {
-    const res = await request(app).get("/");
+const request = require('supertest');
+const app = require('../app');
+const { expect } = require('chai');
+
+describe('GET /', () => {
+  it('should return Hello, CI/CD!', async () => {
+    const res = await request(app).get('/');
     expect(res.status).to.equal(200);
-    expect(res.text).to.equal("Hello, GitHub Actions!");
+    expect(res.text).to.equal('Hello, CI/CD!');  // <-- updated
   });
 });
