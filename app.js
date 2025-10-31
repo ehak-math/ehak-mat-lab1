0f7809bb66c8b012d12e586dc14bfcc935e96bc4
Ehak Math <ehakmath@gmail.com>
2025-10-31 11:11:49 +0700

    Update greeting message

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello, GitHub Actions!");
+  res.send("Hello, CI/CL!");
 });
 module.exports = app;
